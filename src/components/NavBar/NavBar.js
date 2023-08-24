f387e8a6c6a8db65ec211e1642d4629b8e050164
Jade Shi <ziyushi1997@gmail.com>
2023-08-24 15:38:04 -0700

    Add NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import './NavBar.css';
+import { NavLink } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <div className='nav-bar'>
+      <h1 className='name'>Ziyu "Jade" Shi</h1>
+      <h1>Home</h1>
+      <h1>About Me</h1>
+
+    </div>
+  );
+}
