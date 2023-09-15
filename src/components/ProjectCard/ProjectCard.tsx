ce970d61c5a28930af0959d828101428aca890ee
Jade Shi <ziyushi1997@gmail.com>
2023-09-15 11:20:51 -0700

    Add layout to project card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -5,18 +5,21 @@ type props = {
   description: string
 }
 export default function ProjectCard({ imgName, projectName, description }: props) {
+  const logoText = projectName.split(' ').map(word => <h1>{word}</h1>);
   return (
-    <div className='project-cards-container'>
+    <div className='project-card'>
       <div className='bg-container'>
         <img className='featured-project-bg' src={require(`../../Assets/${imgName}.png`)} />
         <div className='overlay'>
-          <h1>{projectName}</h1>
+          <div className='logo-text'>
+            {logoText}
+          </div>
           <div className='project-intro'>
-            <p>{projectName}</p>
+            <h3>{projectName}</h3>
             <p>{description}</p>
             <div className='expand-bar'>
-              <p>READ DETAILS</p>
-              <p>⌄</p>
+              <h4>READ DETAILS</h4>
+              <h4>⌄</h4>
             </div>
           </div>
         </div>
