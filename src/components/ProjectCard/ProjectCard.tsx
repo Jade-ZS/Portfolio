a41c32984865848f6cc31039460ff3c16584ab53
Jade Shi <ziyushi1997@gmail.com>
2023-09-15 15:56:11 -0700

    Flip arrow

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,10 +1,18 @@
 import './ProjectCard.css';
+import { useState } from 'react';
+
 type props = {
   imgName: string,
   projectName: string,
   description: string
 }
+
 export default function ProjectCard({ imgName, projectName, description }: props) {
+  const [expand, setExpand] = useState(false);
+  const handleExpand = () => {
+    setExpand(prev => !prev);
+  }
+
   const logoText = projectName.split(' ').map(word => <h1>{word}</h1>);
   const overlay = (
     <div className='overlay'>
@@ -16,7 +24,7 @@ export default function ProjectCard({ imgName, projectName, description }: props
         <p>{description}</p>
         <div className='expand-bar'>
           <h4>READ DETAILS</h4>
-          <img className='arrow-icon' src={require('../../Assets/down-arrow.png')} alt='arrow'/>
+          <img className={`arrow-icon ${(expand && 'flipped')}`} src={require('../../Assets/down-arrow.png')} alt='arrow'/>
         </div>
       </div>
     </div>
