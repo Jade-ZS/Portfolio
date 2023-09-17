8e9bec7ab2a0c6bad8905a1d726d0075f700083b
Jade Shi <ziyushi1997@gmail.com>
2023-09-17 12:30:05 -0700

    Add hover to round arrow

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,9 +4,15 @@ import { useState } from 'react';
 
 export default function Projects() {
   const [expand, setExpand] = useState(false);
-  const handleExpand = () => {
+  const [hover, setHover] = useState(false);
+  const toggleExpand = () => {
     setExpand(prev => !prev);
+  };
+  const toggleHover = () => {
+    setHover(prev => !prev);
   }
+
+
   const description = 'Lorem ipsum dolor sit amet,  Integer quis auctor elit sed vulputate mi sit amet mauris.'
   return (
     <div className='projects-container'>
@@ -17,8 +23,15 @@ export default function Projects() {
       <ProjectCard projectName='Rancid Tomatillos' description={description} imgName='movie-bg' />
       <div className='more-projects'>
         <div className='expand-bar'>
-          <h4 style={{color: '#8D86BE'}}>{expand ? 'HIDE' : 'VIEW'} MORE</h4>
-          <img onClick={handleExpand} className={`arrow-icon ${(expand && 'flipped')}`} src={require('../../Assets/down-arrow.png')} alt='arrow' />
+          <h4 style={{ color: '#8D86BE' }}>{expand ? 'HIDE' : 'VIEW'} MORE</h4>
+          <img 
+            onClick={toggleExpand} 
+            onMouseEnter={toggleHover} 
+            onMouseLeave={toggleHover}
+            className={`arrow-icon ${(expand && 'toRight')}`} 
+            src={require(`../../Assets/${hover ? 'arrow-right-filled' : 'arrow-right'}.png`)} 
+            alt='arrow' 
+          />
         </div>
         <div className={`accordion ${!expand && 'hidden'}`}>
           <span>accordion</span>
