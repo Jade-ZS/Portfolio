2d4656a78518f5c1926f6924353ca5f217d7ff8a
Jade Shi <ziyushi1997@gmail.com>
2023-09-18 22:34:27 -0700

    Remove comments in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,21 +3,16 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import Home from '../Home/Home';
-import Skills from '../Skills/Skills';
-import Projects from '../Projects/Projects';
 import Portfolio from '../Portfolio/Portfolio';
 import Story from '../Story/Story';
 
 function App() {
-  console.log('pathname: ', window.location.pathname)
   return (
     <div className='App'>
       {<NavBar/ >}
       <Routes>
         <Route path='/'>
           <Route  index element={<Home/>}/>
-          {/* <Route path='skills' element={<Skills/>}/>
-          <Route path='projects' element={<Projects />}/> */}
           <Route path='portfolio' element={<Portfolio />}/>
           <Route path='story' element={<Story />}/>
         </Route>
