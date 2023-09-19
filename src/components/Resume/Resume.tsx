ee2264c87367ffb40a45fc72170309d7a165800d
Jade Shi <ziyushi1997@gmail.com>
2023-09-18 17:07:39 -0700

    Add view pdf

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -4,7 +4,8 @@ export default function Experience() {
   return (
     <div>
       <h1>Resume</h1>
-      <a href={require("./resume.pdf")} download="Jade-Shi-Resume">Download file</a>
+      <a className='view-resume-button' target='blank' href='https://drive.google.com/file/d/1oDQRNW_OSkG0EixDvABJI0e5RtIlxYlT/view?usp=sharing'>View Resume</a>
+      <a className='download-resume-button' href={require("./resume.pdf")} download="Jade-Shi-Resume">Download file</a>
     </div>
   )
 }
