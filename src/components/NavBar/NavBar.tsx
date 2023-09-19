a79f64c8847e5b0a3b67da2611a2904ebaf1dc03
Jade Shi <ziyushi1997@gmail.com>
2023-09-19 08:47:00 -0700

    Add dropdown structure

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -10,7 +10,14 @@ export default function NavBar() {
       <h1 className='name'>Ziyu "Jade" Shi</h1>
       <div className='right'>
         <NavLink to='/'><h1>Home</h1></NavLink>
-        <NavLink to='/portfolio'><h1>Portfolio</h1></NavLink>
+        <div className='dropdown'>
+          <NavLink to='/portfolio'>
+            <h1>Portfolio</h1>
+          </NavLink>
+          <div className='dropdown-menu'>
+            menu
+          </div>
+        </div>
         <NavLink to='/story'><h1>My Story</h1></NavLink>
         <NavLink to='/contact'><h1>Contact</h1></NavLink>
       </div>
