32339f66f76d5012f31b34f3f1010c930e99de94
Jade Shi <ziyushi1997@gmail.com>
2023-09-19 11:28:46 -0700

    Add hover effect to dropdown

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
 import { NavLink, useLocation } from 'react-router-dom';
 
 export default function NavBar() {
   const { pathname } = useLocation();
-
+  const [show, setShow] = useState(false);
+ 
   return (
     <div className={`nav-bar ${pathname.length > 2 && 'shadow'}`}>
       <h1 className='name'>Ziyu "Jade" Shi</h1>
       <div className='right'>
         <NavLink to='/'><h1>Home</h1></NavLink>
-        <div className='dropdown'>
+        <div className='dropdown'
+          onMouseEnter={() => setShow(true)}
+          onMouseLeave={() => setShow(false)}
+        >
           <NavLink to='/portfolio'>
-            <h1>Portfolio</h1>
+            <h1
+              onMouseEnter={() => setShow(true)}
+            >Portfolio</h1>
           </NavLink>
-          <div className='dropdown-menu'>
-            menu
+          <div className={`dropdown-menu ${show && 'show'}`}>
+            <p>Skills</p>
+            <p>Projects</p>
+            <p>Resume</p>
           </div>
         </div>
         <NavLink to='/story'><h1>My Story</h1></NavLink>
