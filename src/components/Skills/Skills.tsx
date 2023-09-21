b77eac5d9e0ab70064c78c7f990f13e76ad93268
Jade Shi <ziyushi1997@gmail.com>
2023-09-20 17:38:28 -0700

    Add skill content

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -6,37 +6,44 @@ export default function Skills() {
       <h1 className='portfolio-header'>Skills</h1>
       <div className='skill-cards-container'>
         <img className='skill-bg' src={require('../../Assets/wave-haikei.png')}/>
-        <div className='skill-card grid-item-1'>
+        <div className='skill-card'>
           <h1>Front End</h1>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
+          <span>React</span>
+          <span>React Native</span>
+          <span>TypeScript</span>
+          <span>Progressive Web Application</span>
+          <span>JavaScript</span>
+          <span>HTML</span>
+          <span>CSS</span>
+          <span>Assessibility Practices</span>
         </div>
-        <div className='skill-card grid-item-2'>
+        <div className='skill-card'>
           <h1>Back End</h1>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
+          <span>Express JS</span>
+          <span>MongoDB</span>
+          <span>SQLite</span>
+          <span>Python</span>
+          <span>Java</span>
         </div>
-        <div className='skill-card grid-item-3'>
+        <div className='skill-card'>
           <h1>UI/UX</h1>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
+          <span>Figma</span>
+          <span>inVision</span>
+          <span>Miro</span>
+          <span>Mobile-First Design</span>
         </div>
-        <div className='skill-card grid-item-4'>
+        <div className='skill-card'>
           <h1>Testing</h1>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
+          <span>Cypress</span>
+          <span>TDD (Mocha/Chai)</span>
+          <span>Accessibility (Lighthouse/Wave)</span>
+        </div>
+        <div className='skill-card'>
+          <h1>Others</h1>
+          <span>Git</span>
+          <span>Agile Practices</span>
+          <span>RESTful APIs</span>
+          <span>Project Management Tools</span>
         </div>
       </div>
     </div>
