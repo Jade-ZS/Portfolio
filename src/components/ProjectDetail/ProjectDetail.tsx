94b46f1d82302631c69a99ad9f82a5479aaf76c6
Jade Shi <ziyushi1997@gmail.com>
2023-09-22 13:19:40 -0700

    Add project detail structure

--- src/components/ProjectDetail/ProjectDetail.tsx
+++ src/components/ProjectDetail/ProjectDetail.tsx
@@ -8,7 +8,21 @@ type props = {
 export default function ProjectDetail({expand} : props) {
   return (
     <div className={`project-detail accordion ${!expand && 'hidden'}`}>
-      <img src={require('../../Assets/placeholder.png')}  style={{height: '3rem'}}/>
+      <div className='content-container'>
+        <h1>Technology Used</h1>
+        <div className='tech-container'>
+          <p>Cypress</p>
+          <p>React</p>
+        </div>
+        <p>Klime is a React Native iOS application designed for rock climbers to create and design their own climbing problems by adding circles to existing climbing wall images. With Klime, users can save, name, and select difficulty levels for their climbing problems. Additionally, upcoming features will allow users to upload pictures and edit created problems.</p>
+        <div className='buttons-to-app'>
+          <button>CODE REPOSITORY</button>
+          <button>LAUNCH THE APP</button>
+        </div>
+      </div>
+      <div className='image-container'>
+        <img src={require('../../Assets/placeholder.png')}  style={{height: '3rem'}}/>
+      </div>
     </div>
   );
 }
