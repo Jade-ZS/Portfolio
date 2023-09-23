f920f2e80d5ac52e43419dd9b58b44f1f3070770
Jade Shi <ziyushi1997@gmail.com>
2023-09-22 18:31:44 -0700

    Refactor styling

--- src/components/ProjectDetail/ProjectDetail.tsx
+++ src/components/ProjectDetail/ProjectDetail.tsx
@@ -9,10 +9,16 @@ export default function ProjectDetail({expand} : props) {
   return (
     <div className={`project-detail accordion ${!expand && 'hidden'}`}>
       <div className='content-container'>
-        <h1>Technology Used</h1>
-        <div className='tech-container'>
-          <p>Cypress</p>
-          <p>React</p>
+        <div className='project-type'>
+          <img className='icon' src={require('../../Assets/collaborator.png')} alt='collaborator icon'/>
+          <span>Team Project</span>
+        </div>
+        <div className='tech-info'>
+          <p>Technology Used</p>
+          <div className='tech-container'>
+            <p>Cypress</p>
+            <p>React</p>
+          </div>
         </div>
         <p>Klime is a React Native iOS application designed for rock climbers to create and design their own climbing problems by adding circles to existing climbing wall images. With Klime, users can save, name, and select difficulty levels for their climbing problems. Additionally, upcoming features will allow users to upload pictures and edit created problems.</p>
         <div className='buttons-to-app'>
