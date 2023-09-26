a4971ad605fab3fc95326efe0083fad87c1751b4
Jade Shi <ziyushi1997@gmail.com>
2023-09-26 08:20:34 -0700

    Add project data

--- src/components/MoreProjects/moreDetails.js
+++ src/components/MoreProjects/moreDetails.js
@@ -1,3 +1,42 @@
+import { FeaturedDetails } from '../../Types';
+
 export const moreDetails = {
-  
+  'cookin': {
+    type: 'Team Project',
+    technologies: ['JavaScript', 'HTML', 'CSS', 'webpack'],
+    image: './moreImages/cookin.png',
+    github: 'https://github.com/Jade-ZS/whats-cookin',
+    deployment: '',
+  },
+  'rock-paper-scissors': {
+    type: 'Solo Project',
+    technologies: ['JavaScript', 'HTML', 'CSS'],
+    image: './moreImages/rps-login.png',
+    github: 'https://github.com/Jade-ZS/Rock-Paper-Scissors',
+    deployment: 'https://rock-paper-scissors-taupe-six.vercel.app/',
+  },
+  'selfcare-center': {
+    type: 'Solo Project',
+    technologies: ['JavaScript', 'HTML', 'CSS'],
+    image: './moreImages/selfcare.png',
+    github: 'https://github.com/Jade-ZS/self-care-center',
+    deployment: 'https://jade-zs.github.io/self-care-center/',
+  },
+  'coloRandom': {
+    type: 'Team Project',
+    technologies: ['JavaScript', 'HTML', 'CSS'],
+    image: './moreImages/coloRandom.png',
+    github: 'https://github.com/Jade-ZS/coloRandom-project',
+    deployment: 'https://alexavanschaardenburg.github.io/coloRandom-project/',
+  },
+  'romcom': {
+    type: 'Pair Project',
+    technologies: ['JavaScript', 'HTML', 'CSS'],
+    image: './moreImages/romcom.png',
+    github: 'https://github.com/Jade-ZS/romcom',
+    deployment: 'https://jade-zs.github.io/romcom/',
+  },
 }
+
+
+
