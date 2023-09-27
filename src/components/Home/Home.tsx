bcbf582f86f73b3bf7c1cf91dd3540bebb764f7a
Jade Shi <ziyushi1997@gmail.com>
2023-09-27 13:13:54 -0700

    Edit profile picture

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -11,8 +11,9 @@ export default function Home() {
   return (
     <div className='home'>
       <div className='intro-container'>
-        <div className='intro-left'>
           <h1>Hi, I'm Ziyu "Jade" Shi!</h1>
+        <img className='profile intro-right' alt='profile image' src={require('../../Assets/profile.JPG')} />
+        <div className='intro-left'>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <div className='contacts-box'>
             <Link to='/'><LinkedInIcon /></Link>
@@ -20,7 +21,6 @@ export default function Home() {
             <Link to='/'><GmailIcon /></Link>
           </div>
         </div>
-        <img className='profile intro-right' alt='profile image' src={require('../../Assets/profile.JPG')} />
       </div>
     </div>
   );
