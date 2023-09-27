4413167e6ddd92fa5f7280d7b59fe6f4f2d92cd5
Jade Shi <ziyushi1997@gmail.com>
2023-09-27 14:59:07 -0700

    Addhamburger menu icon

---
+++ src/Assets/hamburger.tsx
@@ -0,0 +1,22 @@
+export const hamburger = (
+  <svg className='hamburger' style={{height: '2rem', width: '2rem'}} viewBox="0 0 100 100" width="250" id="menu">
+        <rect 
+          className='line top' 
+          width="80" height="10" 
+          x="10" y="25" 
+          rx="5" 
+        />
+        <rect 
+          className='line middle' 
+          width="80" height="10" 
+          x="10" y="45" 
+          rx="5" 
+        />
+        <rect 
+          className='line bottom' 
+          width="80" height="10" 
+          x="10" y="65" 
+          rx="5" 
+        />
+  </svg>
+);
