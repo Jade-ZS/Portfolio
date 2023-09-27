0a7b21ea0875455698eb29ba39fd90816dab203e
Jade Shi <ziyushi1997@gmail.com>
2023-09-26 20:21:41 -0700

    Add skill card

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -10,7 +10,9 @@ export default function Skills() {
             <img className='skill-icon' src={require('../../Assets/design-icon.png')} alt='design icon' />
           </div>
           <h1>UI/UX</h1>
+          <h3>Design Tools:</h3>
           <span>Figma, inVision, Miro</span>
+          <h3>I practice:</h3>
           <span>Mobile-First Design</span>
           <span>Responsive Design</span>
         </div>
@@ -19,11 +21,13 @@ export default function Skills() {
             <img className='skill-icon' src={require('../../Assets/front-end-icon.png')} alt='design icon' />
           </div>
           <h1>Front End</h1>
+          <h3>Languages/Frameworks:</h3>
           <span>JavaScript, HTML, CSS</span>
           <span>React, React Native, TypeScript</span>
+          <h3>User Interface:</h3>
           <span>Progressive Web Application</span>
           <span>Assessibility Practices</span>
-          <h1>Testing</h1>
+          <h3>Testing:</h3>
           <span>Cypress</span>
           <span>TDD (Mocha/Chai)</span>
           <span>Accessibility (Lighthouse/Wave)</span>
@@ -33,16 +37,13 @@ export default function Skills() {
             <img className='skill-icon' src={require('../../Assets/server-icon.png')} alt='design icon' />
           </div>
           <h1>Back End</h1>
-          <span>Express JS</span>
-          <span>MongoDB</span>
-          <span>SQLite</span>
-          <span>Python</span>
-          <span>Java</span>
-          <h1>Others</h1>
-          <span>Git</span>
-          <span>Agile Practices</span>
+          <h3>Languages/Tools:</h3>
+          <span>Express JS, Python</span>
+          <span>Java, SQLite, MongoDB</span>
+          <h3>Others:</h3>
           <span>RESTful APIs</span>
-          <span>Project Management Tools</span>
+          <span>Agile Practices, Git Workflow</span>
+          <span>Project Management</span>
         </div>
       </div>
     </div>
