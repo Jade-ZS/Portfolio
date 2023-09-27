bb5a3206df410f2707dcef37ebab4102a916ae8d
Jade Shi <ziyushi1997@gmail.com>
2023-09-26 20:03:16 -0700

    Add skill icons

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -5,40 +5,39 @@ export default function Skills() {
     <div className='skills-container' id='skills'>
       <h1 className='portfolio-header'>Skills</h1>
       <div className='skill-cards-container'>
-        {/* <img className='skill-bg' src={require('../../Assets/wave-haikei.png')}/> */}
         <div className='skill-card'>
-          <h1>Front End</h1>
-          <span>React</span>
-          <span>React Native</span>
-          <span>TypeScript</span>
-          <span>Progressive Web Application</span>
-          <span>JavaScript</span>
-          <span>HTML</span>
-          <span>CSS</span>
-          <span>Assessibility Practices</span>
-        </div>
-        <div className='skill-card'>
-          <h1>Back End</h1>
-          <span>Express JS</span>
-          <span>MongoDB</span>
-          <span>SQLite</span>
-          <span>Python</span>
-          <span>Java</span>
-        </div>
-        <div className='skill-card'>
+          <div className='skill-icon-container'>
+            <img className='skill-icon' src={require('../../Assets/design-icon.png')} alt='design icon' />
+          </div>
           <h1>UI/UX</h1>
-          <span>Figma</span>
-          <span>inVision</span>
-          <span>Miro</span>
+          <span>Figma, inVision, Miro</span>
           <span>Mobile-First Design</span>
+          <span>Responsive Design</span>
         </div>
         <div className='skill-card'>
+          <div className='skill-icon-container'>
+            <img className='skill-icon' src={require('../../Assets/front-end-icon.png')} alt='design icon' />
+          </div>
+          <h1>Front End</h1>
+          <span>JavaScript, HTML, CSS</span>
+          <span>React, React Native, TypeScript</span>
+          <span>Progressive Web Application</span>
+          <span>Assessibility Practices</span>
           <h1>Testing</h1>
           <span>Cypress</span>
           <span>TDD (Mocha/Chai)</span>
           <span>Accessibility (Lighthouse/Wave)</span>
         </div>
         <div className='skill-card'>
+          <div className='skill-icon-container'>
+            <img className='skill-icon' src={require('../../Assets/server-icon.png')} alt='design icon' />
+          </div>
+          <h1>Back End</h1>
+          <span>Express JS</span>
+          <span>MongoDB</span>
+          <span>SQLite</span>
+          <span>Python</span>
+          <span>Java</span>
           <h1>Others</h1>
           <span>Git</span>
           <span>Agile Practices</span>
