c8c046a65608b139b831aef96c9887b6a8b5b60d
Jade Shi <ziyushi1997@gmail.com>
2023-09-28 08:58:49 -0700

    Clean up

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,14 @@
 import './App.css';
 import React, {useState} from 'react'
 import { Routes, Route } from 'react-router-dom';
-import NavBar from '../NavBar/NavBar';
 import Home from '../Home/Home';
 import Portfolio from '../Portfolio/Portfolio';
-import Story from '../Story/Story';
-import ScrollToHashElement from "../ScrollToHashElement";
 
 function App() {
   const [show, setShow] = useState(false);
 
   return (
     <div className='App'>
-      {/* <ScrollToHashElement show={show}/>
-      <NavBar show={show} setShow={setShow}/> */}
       <Home />
       <Routes>
         <Route path='/' element={<Portfolio />}/>
