b6ea78b89ed8fd03b015c89b31c86fd29a655eb1
Jade Shi <ziyushi1997@gmail.com>
2023-09-28 09:24:53 -0700

    Refactor resume

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -3,17 +3,15 @@ import './Resume.css';
 export default function Experience() {
   return (
     <div className='resume' id='resume'>
-      <div className='resume-button-container'>
-      <img className='illustration' src='https://illustrations.popsy.co/purple/student-going-to-school.svg' alt='student-raising-hand'/>
-        <a className='view-resume-button' target='blank' href='https://drive.google.com/file/d/1oDQRNW_OSkG0EixDvABJI0e5RtIlxYlT/view?usp=sharing'>
-          <img src={require('../../Assets/view.png')}/>
-          <p>View Resume</p>
-        </a>
-        <a className='download-resume-button' href={require("./resume.pdf")} download="Jade-Shi-Resume">
-          <img src={require('../../Assets/download.png')}/>
-          <p>Download Resume</p>
-        </a>
-      </div>
+      <img className='illustration' src='https://illustrations.popsy.co/purple/student-going-to-school.svg' alt='student-raising-hand' />
+      <a className='view-resume-button' target='blank' href='https://drive.google.com/file/d/1oDQRNW_OSkG0EixDvABJI0e5RtIlxYlT/view?usp=sharing'>
+        <img src={require('../../Assets/view.png')} />
+        <p>View Resume</p>
+      </a>
+      <a className='download-resume-button' href={require("./resume.pdf")} download="Jade-Shi-Resume">
+        <img src={require('../../Assets/download.png')} />
+        <p>Download Resume</p>
+      </a>
     </div>
   )
 }
