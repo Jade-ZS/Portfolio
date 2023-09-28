4d3c8c3a0b531f2030ab19ff27044d68ab40a1a4
Jade Shi <ziyushi1997@gmail.com>
2023-09-27 19:39:49 -0700

    Add down arrow

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,6 +5,7 @@ import Projects from '../Projects/Projects';
 import GithubIcon from '../Icons/GithubIcon';
 import GmailIcon from '../Icons/GmailIcon';
 import LinkedInIcon from '../Icons/LinkedInIcon';
+import {downArrow } from '../../Assets/down-arrow'
 
 export default function Home() {
 
@@ -16,10 +17,15 @@ export default function Home() {
           <h1>Hi, I'm Ziyu "Jade" Shi!</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <div className='contacts-box'>
-            <Link to='/'><LinkedInIcon /></Link>
-            <Link to='/'><GithubIcon /></Link>
-            <Link to='/'><GmailIcon /></Link>
+            <a href='https://www.linkedin.com/in/ziyu-jade-shi/' target='blank'><LinkedInIcon /></a>
+            <a href='https://github.com/Jade-ZS' target='blank'><GithubIcon /></a>
+            <a href='mailto:ziyushi1997@gmail.com' target='blank'><GmailIcon /></a>
           </div>
+          <svg className="arrows">
+							<path className="a1" d="M0 0 L30 32 L60 0"></path>
+							<path className="a2" d="M0 20 L30 52 L60 20"></path>
+							<path className="a3" d="M0 40 L30 72 L60 40"></path>
+						</svg>
         </div>
       </div>
     </div>
